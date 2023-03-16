ad7ce3e25c5e6113dd548ba363602ace6ca5238d
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-16 20:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+	const form = document.querySelector('form');
+		const ageInput = document.querySelector('#age');
+		const nameInput = document.querySelector('#name');
+		const btn = document.querySelector('#btn');
+
+		form.addEventListener('submit', (event) => {
+			event.preventDefault();
+
+			const age = ageInput.value;
+			const name = nameInput.value;
+
+			if (!age || !name) {
+				alert('Please enter both age and name.');
+				return;
+			}
+
+			const promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					if (age > 18) {
+						resolve({ age, name });
+					} else {
+						reject({ age, name });
+					}
+				}, 4000);
+			});
+
+			promise
+				.then(({ age, name }) => {
+					alert(`Welcome, ${name}. You can vote.`);
+					return age;
+				})
+				.then((age) => {
+					return { age };
+				})
+				.then((result) => {
+					alert(`Final object: ${JSON.stringify(result)}`);
+				})
+				.catch(({ age, name }) => {
+					alert(`Oh sorry ${name}. You aren't old enough.`);
+				});
+		});
